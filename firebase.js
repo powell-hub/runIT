62eb9252022b4e0a177615c7566de282c210a8a1
powell-hub <powellgbadamosi124@gmail.com>
2026-04-12 10:56:49 +0100

    Add Firebase configuration and initialization

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAMaJI7m-3XG31kMTkVwq5iANQmftynGKk",
+  authDomain: "runit-d3c79.firebaseapp.com",
+  projectId: "runit-d3c79",
+  storageBucket: "runit-d3c79.firebasestorage.app",
+  messagingSenderId: "1006580845621",
+  appId: "1:1006580845621:web:dc6b2658e63e935c87757c"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
